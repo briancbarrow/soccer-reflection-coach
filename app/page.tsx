c6580d08698bad2603d3aee62a8158e2f4905a2b
briancbarrow <briancbarrow@gmail.com>
2025-03-18 17:25:15 -0600

    clean up some boilerplate stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
 "use client"
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import SimpleChatInterface from "@/components/ui/chat-interface";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
